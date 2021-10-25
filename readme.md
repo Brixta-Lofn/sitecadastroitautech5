cf1b49a94a16c222013b271bad794b852e959816
Brixta-Lofn <neidecoutorocha@gmail.com>
2021-10-24 21:54:38 -0300

    Versão original

---
+++ readme.md
@@ -0,0 +1,3 @@
+Tarefa individual do desafio [Itau Tech] Turma 5 PCD
+Rosineide do Couto Rocha
+Formulário de cadastro para clientes com regra de negócios
---
+++ script.js
@@ -0,0 +1,8 @@
+let cadastro = confirm("Deseja continuar na página?")
+
+if(cadastro == true){
+    alert("Que legal, que ficará!")
+
+} else {
+    alert ("Poxa!Volta logo, tá!")
+}
